96ca276466f28026984a586435fb0062815e4a7b
Zach Smith <zsmith126028@gmail.com>
2019-09-11 16:42:14 -0500

    add links to landscape and portrait components, remove prints component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Header from "./components/layout/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Prints from "./components/Prints";
+import Landscape from "./components/photopages/Landscape";
+import Portrait from "./components/photopages/Portrait";
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/prints' component={Prints} />
+            <Route exact path='/prints/landscape' component={Landscape} />
+            <Route exact path='/prints/portrait' component={Portrait} />
             <Route exact path='/about' component={About} />
             <Route exact path='/contact' component={Contact} />
           </Switch>
