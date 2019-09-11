908128f4a73cf997d702fe04d04a2fdc627ae99f
Zach Smith <zsmith126028@gmail.com>
2019-09-11 17:57:30 -0500

    displaying pictures in grid, formatting is very wrong for time being

--- src/components/photopages/LandscapePicture.js
+++ src/components/photopages/LandscapePicture.js
@@ -4,15 +4,44 @@ const LandscapePicture = ({ pictures }) => {
   console.log(pictures);
   return (
     <div className='container'>
-      {pictures.map(picture => (
-        <div key={picture.name} className='card'>
-          <img src={picture.url} alt='things' />
-          <div className='card-body'>
-            <h5 className='card-title'>{picture.name}</h5>
-            <p className='card-text'>{picture.price}</p>
+      <div className='row'>
+        <div className='col-md-4'>
+          {/* {pictures.map(picture => (
+            <div key={picture.name} className='card mb-3'>
+              <img src={picture.url} alt='things' />
+              <div className='card-body'>
+                <h5 className='card-title'>{picture.name}</h5>
+                <p className='card-text'>{picture.price}</p>
+              </div>
+            </div>
+          ))} */}
+          <div key={pictures[0].name} className='card mb-3'>
+            <img src={pictures[0].url} alt='things' />
+            <div className='card-body'>
+              <h5 className='card-title'>{pictures[0].name}</h5>
+              <p className='card-text'>{pictures[0].price}</p>
+            </div>
           </div>
         </div>
-      ))}
+        <div className='col-md-4'>
+          <div key={pictures[1].name} className='card mb-3'>
+            <img src={pictures[1].url} alt='things' />
+            <div className='card-body'>
+              <h5 className='card-title'>{pictures[1].name}</h5>
+              <p className='card-text'>{pictures[1].price}</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div key={pictures[2].name} className='card mb-3'>
+            <img src={pictures[2].url} alt='things' />
+            <div className='card-body'>
+              <h5 className='card-title'>{pictures[2].name}</h5>
+              <p className='card-text'>{pictures[2].price}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
