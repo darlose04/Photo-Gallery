c18cc5cf9d8bd329d0f10d4958f5e7c3877d68de
Zach Smith <zsmith126028@gmail.com>
2019-09-11 11:49:04 -0500

    import Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Photo Gallery</h1>
+      <Header />
     </div>
   );
 }
