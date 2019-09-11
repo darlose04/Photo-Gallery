b845ed6e24ab6c0d46866f5534e40de0b6b75614
Zach Smith <zsmith126028@gmail.com>
2019-09-11 16:42:42 -0500

    add landscape and portrait components

---
+++ src/components/photopages/Landscape.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landscape = () => {
+  return (
+    <div>
+      <h2>This is the page for landscape oriented photos</h2>
+    </div>
+  );
+};
+
+export default Landscape;
---
+++ src/components/photopages/Portrait.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portriat = () => {
+  return (
+    <div>
+      <h2>This is the page for portrait oriented photos</h2>
+    </div>
+  );
+};
+
+export default Portriat;
