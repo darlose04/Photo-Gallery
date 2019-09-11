25a18c3b69e0da65f054e0b749ade3c12d9f9129
Zach Smith <zsmith126028@gmail.com>
2019-09-11 17:48:12 -0500

    import and use pics for hard coded state, only temporary

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
@@ -8,7 +8,56 @@ import Contact from "./components/Contact";
 import Landscape from "./components/photopages/Landscape";
 import Portrait from "./components/photopages/Portrait";
 
+// image imports
+import cottoncandy from "./LoganPhotos/CottonCandySunrise.jpg";
+import railroad from "./LoganPhotos/DesertedRailroad.jpg";
+import grandspring from "./LoganPhotos/GrandPrismaticSpring.jpg";
+import loneeagle from "./LoganPhotos/LoneEagleSunset.jpg";
+import giantshoulders from "./LoganPhotos/OntheShouldersofGiants.jpg";
+import reflections from "./LoganPhotos/Reflections.jpg";
+
 const App = () => {
+  // const [pictures, setPictures] = useState([]);
+
+  const pictures = [
+    {
+      name: "Cotton Candy Sunrise",
+      url: cottoncandy,
+      orientation: "landscape",
+      price: "$25.00"
+    },
+    {
+      name: "Deserted Railroad",
+      url: railroad,
+      orientation: "landscape",
+      price: "$25.00"
+    },
+    {
+      name: "Grand Prismatic Spring",
+      url: grandspring,
+      orientation: "landscape",
+      price: "$25.00"
+    },
+    {
+      name: "Lone Eagle Sunset",
+      url: loneeagle,
+      orientation: "landscape",
+      price: "$25.00"
+    },
+    {
+      name: "On the Shoulders of Giants",
+      url: giantshoulders,
+      orientation: "landscape",
+      price: "$25.00"
+    },
+    {
+      name: "Reflections",
+      url: reflections,
+      orientation: "landscape",
+      price: "$25.00"
+    }
+  ];
+
   return (
     <Router>
       <div className='App'>
@@ -16,7 +65,11 @@ const App = () => {
         <div className='container'>
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/prints/landscape' component={Landscape} />
+            <Route
+              exact
+              path='/prints/landscape'
+              render={props => <Landscape pictures={pictures} />}
+            />
             <Route exact path='/prints/portrait' component={Portrait} />
             <Route exact path='/about' component={About} />
             <Route exact path='/contact' component={Contact} />
