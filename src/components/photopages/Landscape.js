c2e215787749ba62123d9df7ed78d7638daf93d9
Zach Smith <zsmith126028@gmail.com>
2019-09-11 17:38:39 -0500

    import and use LandscapePicture component

--- src/components/photopages/Landscape.js
+++ src/components/photopages/Landscape.js
@@ -1,9 +1,13 @@
 import React from "react";
+import LandscapePicture from "./LandscapePicture";
 
-const Landscape = () => {
+const Landscape = ({ pictures }) => {
+  // console.log(pictures);
+
   return (
     <div>
       <h2>This is the page for landscape oriented photos</h2>
+      <LandscapePicture pictures={pictures} />
     </div>
   );
 };
