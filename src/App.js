0cd018b76be17127240a09afb7ee77a58dccab8a
Zach Smith <zsmith126028@gmail.com>
2019-09-11 13:03:34 -0500

    import react-router-dom and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./components/layout/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Prints from "./components/Prints";
 
 const App = () => {
   return (
-    <div className='App'>
-      <Header />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/portfolio' component={Portfolio} />
+            <Route exact path='/prints' component={Prints} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
