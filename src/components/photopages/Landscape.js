c2b07a321f642e5739f8b9d09d13d81f73b899b1
Zach Smith <zsmith126028@gmail.com>
2019-09-12 12:24:58 -0500

    change name of array for landscape images

--- src/components/photopages/Landscape.js
+++ src/components/photopages/Landscape.js
@@ -1,13 +1,13 @@
 import React from "react";
 import LandscapePicture from "./LandscapePicture";
 
-const Landscape = ({ pictures }) => {
+const Landscape = ({ landscapeImages }) => {
   // console.log(pictures);
 
   return (
     <div className='mt-3'>
       {/* <h2>This is the page for landscape oriented photos</h2> */}
-      <LandscapePicture pictures={pictures} />
+      <LandscapePicture landscapeImages={landscapeImages} />
     </div>
   );
 };
--- src/components/photopages/LandscapePicture.js
+++ src/components/photopages/LandscapePicture.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const LandscapePicture = ({ pictures }) => {
-  console.log(pictures);
+const LandscapePicture = ({ landscapeImages }) => {
+  // console.log(pictures);
   return (
     <div className='container'>
       <div className='row'>
@@ -15,58 +15,82 @@ const LandscapePicture = ({ pictures }) => {
               </div>
             </div>
           ))} */}
-          <div key={pictures[0].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[0].url} alt='things' />
+          <div key={landscapeImages[0].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[0].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[0].name}</h5>
-              <p className='card-text'>{pictures[0].price}</p>
+              <h5 className='card-title'>{landscapeImages[0].name}</h5>
+              <p className='card-text'>{landscapeImages[0].price}</p>
             </div>
           </div>
         </div>
         <div className='col-md-4'>
-          <div key={pictures[1].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[1].url} alt='things' />
+          <div key={landscapeImages[1].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[1].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[1].name}</h5>
-              <p className='card-text'>{pictures[1].price}</p>
+              <h5 className='card-title'>{landscapeImages[1].name}</h5>
+              <p className='card-text'>{landscapeImages[1].price}</p>
             </div>
           </div>
         </div>
         <div className='col-md-4'>
-          <div key={pictures[2].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[2].url} alt='things' />
+          <div key={landscapeImages[2].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[2].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[2].name}</h5>
-              <p className='card-text'>{pictures[2].price}</p>
+              <h5 className='card-title'>{landscapeImages[2].name}</h5>
+              <p className='card-text'>{landscapeImages[2].price}</p>
             </div>
           </div>
         </div>
       </div>
       <div className='row'>
         <div className='col-md-4'>
-          <div key={pictures[3].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[3].url} alt='things' />
+          <div key={landscapeImages[3].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[3].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[3].name}</h5>
-              <p className='card-text'>{pictures[3].price}</p>
+              <h5 className='card-title'>{landscapeImages[3].name}</h5>
+              <p className='card-text'>{landscapeImages[3].price}</p>
             </div>
           </div>
         </div>
         <div className='col-md-4'>
-          <div key={pictures[4].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[4].url} alt='things' />
+          <div key={landscapeImages[4].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[4].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[4].name}</h5>
-              <p className='card-text'>{pictures[4].price}</p>
+              <h5 className='card-title'>{landscapeImages[4].name}</h5>
+              <p className='card-text'>{landscapeImages[4].price}</p>
             </div>
           </div>
         </div>
         <div className='col-md-4'>
-          <div key={pictures[5].name} className='card mb-3'>
-            <img className='img-thumbnail' src={pictures[5].url} alt='things' />
+          <div key={landscapeImages[5].name} className='card mb-3'>
+            <img
+              className='img-thumbnail'
+              src={landscapeImages[5].url}
+              alt='things'
+            />
             <div className='card-body'>
-              <h5 className='card-title'>{pictures[5].name}</h5>
-              <p className='card-text'>{pictures[5].price}</p>
+              <h5 className='card-title'>{landscapeImages[5].name}</h5>
+              <p className='card-text'>{landscapeImages[5].price}</p>
             </div>
           </div>
         </div>
