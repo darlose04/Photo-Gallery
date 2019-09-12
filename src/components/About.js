1cca87cd6efd7b02d2c0d8d49024905429a3c716
Zach Smith <zsmith126028@gmail.com>
2019-09-12 12:04:46 -0500

    create bootstrap card in about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,40 @@
 import React from "react";
+import headshot from "../LoganPhotos/FullSizeRender.jpg";
 
 const About = () => {
+  const cardStyle = {
+    maxWidth: 1050
+  };
+
   return (
-    <div>
-      <h1>This is going to be the About page</h1>
+    <div className='mt-3'>
+      <div className='card' style={cardStyle}>
+        <div className='row no-gutters'>
+          <div className='col-md-6'>
+            <img className='img-thumbnail mt-3' src={headshot} alt='headshot' />
+          </div>
+          <div className='col-md-6'>
+            <div className='card-body'>
+              <h4 className='card-title'>About Logan</h4>
+              <p className='card-text'>
+                My name is Logan Wilson and I am just a twenty-something trying
+                to turn my hobby of photography into something more. I have my
+                dad to thank for getting me interested in photography at a young
+                age. Every family vacation, holiday, birthday, and sporting
+                event, my dad would be the photographer. Capturing those moments
+                was important to my dad, no matter how much my brother and I
+                complained while posing for the thousandth picture on one of our
+                childhood vacations. As I got older though, I began to
+                appreciate what my dad did and why he did it. Capturing a moment
+                is something special. Even the best of memories will ade with
+                age, but a picture can spark that memory and bring back all the
+                feelings that come with it. There is nothing more magical than
+                freezing a moment in time and being able to keep it forever.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
