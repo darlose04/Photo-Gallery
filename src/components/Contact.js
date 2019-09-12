e44a92b144b7ce4a0622c3f3fe7c864922a554f5
Zach Smith <zsmith126028@gmail.com>
2019-09-12 16:35:21 -0500

    create modal for contact information instead of a separate page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,49 @@
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-const Contact = () => {
+const Contact = ({ show, handleClose }) => {
+  const ulStyle = {
+    listStyle: "none"
+  };
+
+  const aStyle = {
+    color: "black"
+  };
+
   return (
     <div>
-      <h1>This is the contact page</h1>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact Logan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul style={ulStyle}>
+            <li>lcwphotos@gmail.com</li>
+            <li>
+              <a
+                style={aStyle}
+                href='https://www.instagram.com/loganwilsonphotography/'
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a
+                style={aStyle}
+                href='https://www.facebook.com/LoganWilsonPhoto/'
+              >
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
