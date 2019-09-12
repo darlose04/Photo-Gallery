2b1d3767a1228449a3d7095d8d10c146fc4c704d
Zach Smith <zsmith126028@gmail.com>
2019-09-12 15:38:23 -0500

    add new component to display all the pictures with a react photo gallery library

---
+++ src/components/photopages/AllPictures.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllPictures = ({ allPictures }) => {
+  return (
+    <div>
+      <h1>This is where I'm going to try a photo gallery react library</h1>
+    </div>
+  );
+};
+
+export default AllPictures;
