3d42bb349bb42ff22d5189189b47b829a4043b00
Zach Smith <zsmith126028@gmail.com>
2019-09-12 17:20:37 -0500

    add Footer to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -300,6 +301,7 @@ const App = () => {
             <Route exact path='/contact' component={Contact} />
           </Switch>
         </div>
+        <Footer />
       </div>
     </Router>
   );
