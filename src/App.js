9ac4f833fa3a3d5266a95e37ea800759ef27da01
Zach Smith <zsmith126028@gmail.com>
2019-09-16 16:03:07 -0500

    comment out App.css in order to import css in different component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 
 import Header from "./components/layout/Header";
 // import Footer from "./components/layout/Footer";
