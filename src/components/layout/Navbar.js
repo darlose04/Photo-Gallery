ddfe938142d21f4ce351dbc5a32ff715ae770a9c
Zach Smith <zsmith126028@gmail.com>
2019-09-19 18:51:13 -0500

    bring in Contact modal

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,63 +3,71 @@ import Contact from "../Contact";
 
 const Navbar = ({ show, handleClose, handleShow }) => {
   return (
-    <nav class='navbar sticky-top navbar-expand-sm navbar-light mb-2'>
-      <div class='container'>
-        <a class='navbar-brand' href='/'>
-          Logan Wilson Photography
-        </a>
-        <button
-          class='navbar-toggler'
-          data-toggle='collapse'
-          data-target='#navbarNav'
-        >
-          <span class='navbar-toggler-icon'></span>
-        </button>
-        <div id='navbarNav' class='collapse navbar-collapse'>
-          <ul class='navbar-nav'>
-            <li class='nav-item '>
-              <a class='nav-link' href='/'>
-                Home
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='/portfolio'>
-                Portfolio
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a href='/about' className='nav-link'>
-                About
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link' onClick={handleShow} href='#'>
-                Contact
-                <Contact show={show} handleClose={handleClose} />
-              </a>
-            </li>
-          </ul>
-          <ul className='navbar-nav ml-auto'>
-            <li className='nav-item'>
-              <a
-                href='https://www.instagram.com/loganwilsonphotography/'
-                className='nav-link'
-              >
-                <i className='fab fa-instagram fa-2x'></i>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a
-                href='https://www.facebook.com/LoganWilsonPhoto/'
-                className='nav-link'
-              >
-                <i className='fab fa-facebook fa-2x'></i>
-              </a>
-            </li>
-          </ul>
+    <div>
+      <nav className='navbar sticky-top navbar-expand-sm navbar-light mb-2'>
+        <div className='container'>
+          <a className='navbar-brand' href='/'>
+            Logan Wilson Photography
+          </a>
+          <button
+            className='navbar-toggler'
+            data-toggle='collapse'
+            data-target='#navbarNav'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div id='navbarNav' className='collapse navbar-collapse'>
+            <ul className='navbar-nav'>
+              <li className='nav-item '>
+                <a className='nav-link' href='/'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='/portfolio'>
+                  Portfolio
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a href='/about' className='nav-link'>
+                  About
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  className='nav-link'
+                  onClick={handleShow}
+                  href='#'
+                  data-toggle='modal'
+                  data-target='#exampleModal'
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <a
+                  href='https://www.instagram.com/loganwilsonphotography/'
+                  className='nav-link'
+                >
+                  <i className='fab fa-instagram fa-2x'></i>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a
+                  href='https://www.facebook.com/LoganWilsonPhoto/'
+                  className='nav-link'
+                >
+                  <i className='fab fa-facebook fa-2x'></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Contact show={show} handleClose={handleClose} handleShow={handleShow} />
+    </div>
   );
 };
 
