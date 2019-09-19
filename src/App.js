6e55b320b041c1536b1c44047cdd91147e4a6210
Zach Smith <zsmith126028@gmail.com>
2019-09-19 11:30:40 -0500

    import App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import "./App.css";
+import "./styles/App.css";
 
 // import Header from "./components/layout/Header";
 import Navbar from "./components/layout/Navbar";
