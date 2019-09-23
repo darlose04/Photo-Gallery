13fb0a8f505a155c0372a4a191f302b8720eacaf
Zach Smith <zsmith126028@gmail.com>
2019-09-23 15:47:52 -0500

    probably not going to use the ImageSelection component, but not going to delete just yet

--- src/components/photopages/ImageSelection.js
+++ src/components/photopages/ImageSelection.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import LargeImage from "./LargeImage";
 
 const ImageSelection = ({ photo }) => {
   const cont = {
@@ -15,6 +16,50 @@ const ImageSelection = ({ photo }) => {
     ...cont
   };
 
+  const imageModal = () => {
+    // console.log(`${photo.name} clicked`);
+    return (
+      <div
+        className='modal fade'
+        id='exampleModalCenter'
+        tabIndex='-1'
+        role='dialog'
+        aria-labelledby='exampleModalCenterTitle'
+        aria-hidden='true'
+      >
+        <div className='modal-dialog modal-dialog-centered' role='document'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title' id='exampleModalCenterTitle'>
+                {photo.name}
+              </h5>
+              <button
+                type='button'
+                className='close'
+                data-dismiss='modal'
+                aria-label='Close'
+              >
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div className='modal-body'>
+              <img className='img-fluid' src={photo.src} alt={photo.name} />
+            </div>
+            <div className='modal-footer'>
+              <button
+                type='button'
+                className='btn btn-secondary'
+                data-dismiss='modal'
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const handleImage = () => {
     console.log(`The image named ${photo.name} was clicked`);
   };
@@ -25,9 +70,13 @@ const ImageSelection = ({ photo }) => {
         style={imageStyle}
         className='image-selection'
         src={photo.src}
-        alt={photo.title}
+        alt={photo.name}
         onClick={handleImage}
+        // onClick={imageModal}
+        data-toggle='modal'
+        data-target='#exampleModalCenter'
       />
+      {imageModal()}
       <style>{`.image-selection:hover{outline:2px solid #06befa}`}</style>
     </div>
   );
