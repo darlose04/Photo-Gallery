e12d047f3d6422f1a6ae502a54744cc00a2031e3
Zach Smith <zsmith126028@gmail.com>
2019-10-30 14:34:48 -0500

    try using different source for photos

--- src/photos.js
+++ src/photos.js
@@ -1,193 +1,8 @@
-// const photos = [
-//     {
-//       id: 1,
-//       name: "Cotton Candy Sunrise",
-//       src: "http://lcwphotos.imgix.net/CottonCandySunrise.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 2,
-//       name: "Deserted Railroad",
-//       src: "http://lcwphotos.imgix.net/DesertedRailroad.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 3,
-//       name: "Snake River Overlook",
-//       src: "http://lcwphotos.imgix.net/SnakeRiverOverlook.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 4
-//     },
-//     {
-//       id: 4,
-//       name: "Grand Prismatic Spring",
-//       src: "http://lcwphotos.imgix.net/GrandPrismaticSpring.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 5,
-//       name: "Lone Eagle Sunset",
-//       src: "http://lcwphotos.imgix.net/LoneEagleSunset.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 6,
-//       name: "On the Shoulders of Giants",
-//       src: "http://lcwphotos.imgix.net/OntheShouldersofGiants.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 7,
-//       name: "Reflections",
-//       src: "http://lcwphotos.imgix.net/Reflections.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 8,
-//       name: "Chasing Waterfalls",
-//       src: "http://lcwphotos.imgix.net/ChasingWaterfalls.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 4
-//     },
-
-//     {
-//       id: 9,
-//       name: "Last Light",
-//       src: "http://lcwphotos.imgix.net/LastLight.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 4
-//     },
-
-//     {
-//       id: 10,
-//       name: "Rustic Barn",
-//       src: "http://lcwphotos.imgix.net/RusticBarn.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 11,
-//       name: "The Archway to the West",
-//       src: "http://lcwphotos.imgix.net/TheArchwayToTheWest.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 4
-//     },
-//     {
-//       id: 12,
-//       name: "Schwabacher Sunrise",
-//       src: "http://lcwphotos.imgix.net/SchwabacherSunrise.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 13,
-//       name: "Eye to Eye",
-//       src: "http://lcwphotos.imgix.net/EyeToEye.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 5
-//     },
-//     {
-//       id: 14,
-//       name: "Switzerland of America",
-//       src: "http://lcwphotos.imgix.net/SwitzerlandofAmerica.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 15,
-//       name: "The Upside Down",
-//       src: "http://lcwphotos.imgix.net/TheUpsideDown.jpg",
-//       orientation: "portrait",
-//       price: "$25.00",
-//       width: 3,
-//       height: 5
-//     },
-//     {
-//       id: 16,
-//       name: "Teton Sunrise",
-//       src: "http://lcwphotos.imgix.net/TetonSunrise.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 17,
-//       name: "The Hazy Bells",
-//       src: "http://lcwphotos.imgix.net/TheHazyBells.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 18,
-//       name: "The Majestic Flatirons",
-//       src: "http://lcwphotos.imgix.net/TheMajesticFlatirons.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 19,
-//       name: "The Window to the West",
-//       src: "http://lcwphotos.imgix.net/TheWindowToTheWest.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     },
-//     {
-//       id: 20,
-//       name: "Colors",
-//       src: "http://localhost:3003/images/image/84b5e5170a29fcaf4da58c2f9d89b5cf.jpg",
-//       orientation: "landscape",
-//       price: "$25.00",
-//       width: 4,
-//       height: 3
-//     }
-//   ];
-
 const photos = [
     {
       id: 1,
       name: "Cotton Candy Sunrise",
-      src: "https://lcwphotography.herokuapp.com/images/image/2923d7026c72c1d01599df15464105d8.jpg",
+      src: "http://lcwphotos.imgix.net/CottonCandySunrise.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -196,7 +11,7 @@ const photos = [
     {
       id: 2,
       name: "Deserted Railroad",
-      src: "https://lcwphotography.herokuapp.com/images/image/14ea549076394ed5b6f8e233edb0ad7c.jpg",
+      src: "http://lcwphotos.imgix.net/DesertedRailroad.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -205,7 +20,7 @@ const photos = [
     {
       id: 3,
       name: "Snake River Overlook",
-      src: "https://lcwphotography.herokuapp.com/images/image/1dcbfa783a69fadfee9c52605e632f65.jpg",
+      src: "http://lcwphotos.imgix.net/SnakeRiverOverlook.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -214,7 +29,7 @@ const photos = [
     {
       id: 4,
       name: "Grand Prismatic Spring",
-      src: "https://lcwphotography.herokuapp.com/images/image/f3e812eea60616e3bb0c178340c3dbe2.jpg",
+      src: "http://lcwphotos.imgix.net/GrandPrismaticSpring.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -223,7 +38,7 @@ const photos = [
     {
       id: 5,
       name: "Lone Eagle Sunset",
-      src: "https://lcwphotography.herokuapp.com/images/image/4c902b4ecdb88806e1954520291bca3d.jpg",
+      src: "http://lcwphotos.imgix.net/LoneEagleSunset.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -232,7 +47,7 @@ const photos = [
     {
       id: 6,
       name: "On the Shoulders of Giants",
-      src: "https://lcwphotography.herokuapp.com/images/image/e26fdbaeab6b8358b88868ee87244fdb.jpg",
+      src: "http://lcwphotos.imgix.net/OntheShouldersofGiants.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -241,7 +56,7 @@ const photos = [
     {
       id: 7,
       name: "Reflections",
-      src: "https://lcwphotography.herokuapp.com/images/image/f88b06ad6128193d37afaa4e3fb69bf5.jpg",
+      src: "http://lcwphotos.imgix.net/Reflections.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -250,7 +65,7 @@ const photos = [
     {
       id: 8,
       name: "Chasing Waterfalls",
-      src: "https://lcwphotography.herokuapp.com/images/image/2fdde5562ba17a091bb15413d6c467da.jpg",
+      src: "http://lcwphotos.imgix.net/ChasingWaterfalls.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -260,7 +75,7 @@ const photos = [
     {
       id: 9,
       name: "Last Light",
-      src: "https://lcwphotography.herokuapp.com/images/image/5b0bfb8047fe0f422a34221248ce9da3.jpg",
+      src: "http://lcwphotos.imgix.net/LastLight.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -270,7 +85,7 @@ const photos = [
     {
       id: 10,
       name: "Rustic Barn",
-      src: "https://lcwphotography.herokuapp.com/images/image/af5069e35fec2b40b54f4a0c4b7564b1.jpg",
+      src: "http://lcwphotos.imgix.net/RusticBarn.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -279,7 +94,7 @@ const photos = [
     {
       id: 11,
       name: "The Archway to the West",
-      src: "https://lcwphotography.herokuapp.com/images/image/ea28056f5a45aaf7b8aae0cd652b4543.jpg",
+      src: "http://lcwphotos.imgix.net/TheArchwayToTheWest.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -288,7 +103,7 @@ const photos = [
     {
       id: 12,
       name: "Schwabacher Sunrise",
-      src: "https://lcwphotography.herokuapp.com/images/image/71a20571c3a60a2db16fe791adc2e820.jpg",
+      src: "http://lcwphotos.imgix.net/SchwabacherSunrise.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -297,7 +112,7 @@ const photos = [
     {
       id: 13,
       name: "Eye to Eye",
-      src: "https://lcwphotography.herokuapp.com/images/image/6e68fe9a27331d6a3104809c711a4746.jpg",
+      src: "http://lcwphotos.imgix.net/EyeToEye.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -306,7 +121,7 @@ const photos = [
     {
       id: 14,
       name: "Switzerland of America",
-      src: "https://lcwphotography.herokuapp.com/images/image/20aecc7b545c29f82d00108680899b93.jpg",
+      src: "http://lcwphotos.imgix.net/SwitzerlandofAmerica.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -315,7 +130,7 @@ const photos = [
     {
       id: 15,
       name: "The Upside Down",
-      src: "https://lcwphotography.herokuapp.com/images/image/d8ff9185342d23db842209d9d481fe66.jpg",
+      src: "http://lcwphotos.imgix.net/TheUpsideDown.jpg",
       orientation: "portrait",
       price: "$25.00",
       width: 3,
@@ -324,7 +139,7 @@ const photos = [
     {
       id: 16,
       name: "Teton Sunrise",
-      src: "https://lcwphotography.herokuapp.com/images/image/9571763a0369c9df849c98243a9613ac.jpg",
+      src: "http://lcwphotos.imgix.net/TetonSunrise.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -333,7 +148,7 @@ const photos = [
     {
       id: 17,
       name: "The Hazy Bells",
-      src: "https://lcwphotography.herokuapp.com/images/image/98f7376713206d1a4ace942c58c8c94b.jpg",
+      src: "http://lcwphotos.imgix.net/TheHazyBells.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -342,7 +157,7 @@ const photos = [
     {
       id: 18,
       name: "The Majestic Flatirons",
-      src: "https://lcwphotography.herokuapp.com/images/image/b0ba6b75aaebff910e034c270a6e332f.jpg",
+      src: "http://lcwphotos.imgix.net/TheMajesticFlatirons.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
@@ -351,12 +166,188 @@ const photos = [
     {
       id: 19,
       name: "The Window to the West",
-      src: "https://lcwphotography.herokuapp.com/images/image/5a872fefc6740bc7367a6586d1e6fc1f.jpg",
+      src: "http://lcwphotos.imgix.net/TheWindowToTheWest.jpg",
       orientation: "landscape",
       price: "$25.00",
       width: 4,
       height: 3
     }
   ];
+
+// const photos = [
+//     {
+//       id: 1,
+//       name: "Cotton Candy Sunrise",
+//       src: "https://lcwphotography.herokuapp.com/images/image/2923d7026c72c1d01599df15464105d8.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 2,
+//       name: "Deserted Railroad",
+//       src: "https://lcwphotography.herokuapp.com/images/image/14ea549076394ed5b6f8e233edb0ad7c.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 3,
+//       name: "Snake River Overlook",
+//       src: "https://lcwphotography.herokuapp.com/images/image/1dcbfa783a69fadfee9c52605e632f65.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       id: 4,
+//       name: "Grand Prismatic Spring",
+//       src: "https://lcwphotography.herokuapp.com/images/image/f3e812eea60616e3bb0c178340c3dbe2.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 5,
+//       name: "Lone Eagle Sunset",
+//       src: "https://lcwphotography.herokuapp.com/images/image/4c902b4ecdb88806e1954520291bca3d.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 6,
+//       name: "On the Shoulders of Giants",
+//       src: "https://lcwphotography.herokuapp.com/images/image/e26fdbaeab6b8358b88868ee87244fdb.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 7,
+//       name: "Reflections",
+//       src: "https://lcwphotography.herokuapp.com/images/image/f88b06ad6128193d37afaa4e3fb69bf5.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 8,
+//       name: "Chasing Waterfalls",
+//       src: "https://lcwphotography.herokuapp.com/images/image/2fdde5562ba17a091bb15413d6c467da.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 4
+//     },
+
+//     {
+//       id: 9,
+//       name: "Last Light",
+//       src: "https://lcwphotography.herokuapp.com/images/image/5b0bfb8047fe0f422a34221248ce9da3.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 4
+//     },
+
+//     {
+//       id: 10,
+//       name: "Rustic Barn",
+//       src: "https://lcwphotography.herokuapp.com/images/image/af5069e35fec2b40b54f4a0c4b7564b1.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 11,
+//       name: "The Archway to the West",
+//       src: "https://lcwphotography.herokuapp.com/images/image/ea28056f5a45aaf7b8aae0cd652b4543.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 4
+//     },
+//     {
+//       id: 12,
+//       name: "Schwabacher Sunrise",
+//       src: "https://lcwphotography.herokuapp.com/images/image/71a20571c3a60a2db16fe791adc2e820.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 13,
+//       name: "Eye to Eye",
+//       src: "https://lcwphotography.herokuapp.com/images/image/6e68fe9a27331d6a3104809c711a4746.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 5
+//     },
+//     {
+//       id: 14,
+//       name: "Switzerland of America",
+//       src: "https://lcwphotography.herokuapp.com/images/image/20aecc7b545c29f82d00108680899b93.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 15,
+//       name: "The Upside Down",
+//       src: "https://lcwphotography.herokuapp.com/images/image/d8ff9185342d23db842209d9d481fe66.jpg",
+//       orientation: "portrait",
+//       price: "$25.00",
+//       width: 3,
+//       height: 5
+//     },
+//     {
+//       id: 16,
+//       name: "Teton Sunrise",
+//       src: "https://lcwphotography.herokuapp.com/images/image/9571763a0369c9df849c98243a9613ac.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 17,
+//       name: "The Hazy Bells",
+//       src: "https://lcwphotography.herokuapp.com/images/image/98f7376713206d1a4ace942c58c8c94b.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 18,
+//       name: "The Majestic Flatirons",
+//       src: "https://lcwphotography.herokuapp.com/images/image/b0ba6b75aaebff910e034c270a6e332f.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     },
+//     {
+//       id: 19,
+//       name: "The Window to the West",
+//       src: "https://lcwphotography.herokuapp.com/images/image/5a872fefc6740bc7367a6586d1e6fc1f.jpg",
+//       orientation: "landscape",
+//       price: "$25.00",
+//       width: 4,
+//       height: 3
+//     }
+//   ];
 
 export default photos;
