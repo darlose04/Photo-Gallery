3dac11a3d4b7ccc3cdda36f139599d139e8f863f
Zach Smith <zsmith126028@gmail.com>
2019-11-15 18:24:05 -0600

    deciding whether or not to create separate component for Purchase form. trying to implement Stripe

--- src/components/Purchases.js
+++ src/components/Purchases.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Information from "./Information";
+import { Elements, injectStripe } from "react-stripe-elements";
 
 const Purchases = ({ photos }) => {
   const [source, setSource] = useState(
@@ -34,38 +35,39 @@ const Purchases = ({ photos }) => {
               </h2>
               <p className="card-text font-weight-bold">$25.00+</p>
               <p className="card-text">Free shipping to the United States</p>
-
-              <form>
-                <label className="card-text">Photos</label>
-                <div className="form-group">
-                  <select
-                    id="photo-value"
-                    className="form-control mb-3"
-                    onChange={photoChange}
-                  >
-                    <option>Select a Photo</option>
-                    {photoSelection()}
-                  </select>
-                </div>
-                <label className="card-text">Style</label>
-                <div className="form-group">
-                  <select className="form-control">
-                    <option>Select a Style</option>
-                    <option>8x10 Fine Art Print ($25.00)</option>
-                    <option>8x10 Metal Print ($70.00)</option>
-                    <option>11x14 Fine Art Print ($35.00)</option>
-                    <option>11x14 Canvas Print ($65.00)</option>
-                    <option>11x14 Metal Print ($105.00)</option>
-                    <option>16x20 Fine Art Print ($60.00)</option>
-                    <option>16x20 Canvas Print ($80.00)</option>
-                    <option>16X20 Metal Print ($185.00)</option>
-                    <option>24x36 Canvas Print ($150.00)</option>
-                  </select>
-                </div>
-                <button type="submit" className="btn btn-success btn-block">
-                  Purchase
-                </button>
-              </form>
+              <Elements>
+                <form>
+                  <label className="card-text">Photos</label>
+                  <div className="form-group">
+                    <select
+                      id="photo-value"
+                      className="form-control mb-3"
+                      onChange={photoChange}
+                    >
+                      <option>Select a Photo</option>
+                      {photoSelection()}
+                    </select>
+                  </div>
+                  <label className="card-text">Style</label>
+                  <div className="form-group">
+                    <select className="form-control">
+                      <option>Select a Style</option>
+                      <option>8x10 Fine Art Print ($25.00)</option>
+                      <option>8x10 Metal Print ($70.00)</option>
+                      <option>11x14 Fine Art Print ($35.00)</option>
+                      <option>11x14 Canvas Print ($65.00)</option>
+                      <option>11x14 Metal Print ($105.00)</option>
+                      <option>16x20 Fine Art Print ($60.00)</option>
+                      <option>16x20 Canvas Print ($80.00)</option>
+                      <option>16X20 Metal Print ($185.00)</option>
+                      <option>24x36 Canvas Print ($150.00)</option>
+                    </select>
+                  </div>
+                  <button type="submit" className="btn btn-success btn-block">
+                    Purchase
+                  </button>
+                </form>
+              </Elements>
             </div>
           </div>
           <div className="col-md-6">
