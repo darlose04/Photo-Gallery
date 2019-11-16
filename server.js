9da7cffd6a7173e6c828cb3dcae6fdd2dd788791
Zach Smith <zsmith126028@gmail.com>
2019-11-16 14:21:29 -0600

    begin stripe backend file

---
+++ server.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+const uuid = require("uuid/v4");
+const config = require("./config");
+const stripe = require("stripe")(config.SECRET_KEY);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Add the Stripe Secret Key to the .require('stripe') statement");
+});
+
+const PORT = app.listen();
