028ef318482d1bcb74d68b6a95f55291fb49dcc5
Zach Smith <zsmith126028@gmail.com>
2019-11-16 14:21:02 -0600

    update var name

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 let PORT = process.env.PORT;
-let SECRET_KEY = process.env.SECRET_KEY;
+let SECRET_TEST_KEY = process.env.SECRET_TEST_KEY;
 
 module.exports = {
   PORT,
-  SECRET_KEY
+  SECRET_TEST_KEY
 };
