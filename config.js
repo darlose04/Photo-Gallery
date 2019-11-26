d6e6e6d5b03aab4fb86d4bc9685d0111eb97dca6
Zach Smith <zsmith126028@gmail.com>
2019-11-26 16:51:08 -0600

    live key not working

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 
-let PORT = process.env.PORT;
 let SECRET_TEST_KEY = process.env.SECRET_TEST_KEY;
 let SECRET_LIVE_KEY = process.env.SECRET_LIVE_KEY;
 
 module.exports = {
-  PORT,
   SECRET_TEST_KEY,
   SECRET_LIVE_KEY
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const uuid = require("uuid/v4");
 const config = require("./config");
-const stripe = require("stripe")(config.SECRET_TEST_KEY);
+const stripe = require("stripe")(config.SECRET_LIVE_KEY);
 
 const app = express();
 
