e16a0df3c947d640fe9a72d11225cbab8f31e989
Zach Smith <zsmith126028@gmail.com>
2020-03-30 10:07:23 -0500

    remove sale

--- client/src/components/Purchases.js
+++ client/src/components/Purchases.js
@@ -102,7 +102,7 @@ const Purchases = ({ photos }) => {
   };
 
   let productPrice = parseInt(productDetails.split(",")[2]);
-  let salePrice = productPrice * 0.8;
+  // let salePrice = productPrice * 0.8;
   let productSize = productDetails.split(",")[0];
   let productType = productDetails.split(",")[1];
 
@@ -113,7 +113,6 @@ const Purchases = ({ photos }) => {
         token,
         productName,
         productPrice,
-        salePrice,
         productSize,
         productType
       }
@@ -138,9 +137,9 @@ const Purchases = ({ photos }) => {
               <h2 className="card-title card-about">
                 Photo Selection & Purchase
               </h2>
-              <div className="alert alert-success text-uppercase">
+              {/* <div className="alert alert-success text-uppercase">
                 Spring 2020 Sale! 20% off through March 29th!
-              </div>
+              </div> */}
               <p className="card-text">Free shipping to the United States</p>
               <form>
                 <label className="card-text">Photos</label>
@@ -177,11 +176,11 @@ const Purchases = ({ photos }) => {
                   token={handleToken}
                   billingAddress
                   shippingAddress
-                  amount={salePrice * 100}
+                  amount={productPrice * 100}
                   name={productName}
                 >
                   <button type="submit" className="btn btn-success btn-block">
-                    Purchase for <del>${productPrice}</del> ${salePrice}
+                    Purchase for ${productPrice}
                   </button>
                 </StripeCheckout>
               )}
