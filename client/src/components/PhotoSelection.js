de740e1a6d29ee9f0f9cea21bff210dde3ce5374
Zach Smith <zsmith126028@gmail.com>
2020-12-04 11:30:06 -0600

    add new component where the photo selection and purchase will happen

---
+++ client/src/components/PhotoSelection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotoSelection = () => {
+  return (
+    <div>
+      <h1>This is where I will move the Photo purchase section</h1>
+    </div>
+  );
+};
+
+export default PhotoSelection;
