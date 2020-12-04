8b55dcbe71fc536907c6081b39461532931f8ba6
Zach Smith <zsmith126028@gmail.com>
2020-12-04 12:18:01 -0600

    move purchasesStyle function here and pass down as props

--- client/src/components/Purchases.js
+++ client/src/components/Purchases.js
@@ -4,9 +4,20 @@ import PhotoSelection from "./PhotoSelection";
 import Calendar from "./Calendar";
 
 const Purchases = ({ photos }) => {
+  const [purchaseSelection, setPurchaseSelection] = useState("");
+
+  const purchasesStyle = {
+    maxWidth: 1100,
+    backgroundColor: "rgba(0,0,0,0.8)",
+  };
+
+  const purchaseSelectionChange = (event) => {
+    setPurchaseSelection(event.target.innerText);
+  };
+
   return (
     <div className="container mb-5">
-      <PhotoSelection photos={photos} />
+      <PhotoSelection photos={photos} purchasesStyle={purchasesStyle} />
       <Information />
     </div>
   );
