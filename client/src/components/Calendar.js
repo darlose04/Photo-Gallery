6887b41d68c6a4ff5f3054d345ff328a68378334
Zach Smith <zsmith126028@gmail.com>
2020-12-04 13:08:18 -0600

    add price and name for product in stripe checkout

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,9 +1,51 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
-const Calendar = () => {
+const Calendar = ({ purchasesStyle }) => {
+  let productPrice = 25;
+  let productName = "Calendar";
+
+  const handleToken = async (token, addresses) => {
+    const response = await axios.post(
+      "https://logancwilsonphotography.herokuapp.com/checkout",
+      {
+        token,
+        productName,
+        productPrice,
+      }
+    );
+  };
+
   return (
-    <div>
-      <h1>This will be the option for purchasing a calendar</h1>
+    <div className="card about pr-2" style={purchasesStyle}>
+      <div className="row no-gutters">
+        <div className="col-md-6">
+          <div className="card-body">
+            <h2 className="card-title card-about">Calendar Purchase</h2>
+            <p className="card-text">Free shipping to the United States</p>
+            <StripeCheckout
+              stripeKey="pk_live_NfUn28zVGn82Y5WW2exzyyRw00ygY2yGyI"
+              token={handleToken}
+              billingAddress
+              shippingAddress
+              amount={productPrice * 100}
+              name={productName}
+            >
+              <button type="submit" className="btn btn-success btn-block">
+                Purchase for ${productPrice}
+              </button>
+            </StripeCheckout>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <img
+            className="header rounded float-right mt-3 mr-2 mb-3"
+            src="../photos/LCWCalendar.jpg"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
